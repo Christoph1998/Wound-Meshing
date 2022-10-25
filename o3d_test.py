278d0b7238703c1afb1815120cbea9f7653eea61
Christoph1998 <christoph@zoell.de>
2022-10-25 23:36:39 +0200

    open3d ist installiert

--- o3d_test.py
+++ o3d_test.py
@@ -1,6 +1,8 @@
 import open3d as o3d
 import numpy as np
 
+print("New line of Code")
+
 print("Load a ply point cloud, print it, and render it")
 ply_point_cloud = o3d.data.PLYPointCloud()
 pcd = o3d.io.read_point_cloud(ply_point_cloud.path)
