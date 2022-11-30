b697a084b0d89c612bb929d24f42a385f68c245c
Christoph1998 <christoph@zoell.de>
2022-11-30 16:53:18 +0100

    good crop and mesh analysis

--- cropper.py
+++ cropper.py
@@ -23,62 +23,6 @@ def display_inlier_outlier(cloud, ind):
 
 
 if __name__ == "__main__":
-    
-    # Parameter 
-    CAMERA_INTRINSICS = np.array([[797.965087890625, 0.0, 630.5733642578125], [0.0, 797.965087890625, 389.7864685058594], [0.0, 0.0, 1.0]],dtype=np.float64)
-    REMOVAL = "radius" # "statistical"
-    ZMIN = 100 # mm
-    ZMAX = 355 # mm 
-
-    # load test arrays
-    depth_map_in = np.load(r"SampleData\depth_14-11-2022_14-42-13.npy")
-    disparity_map_in = np.load(r"SampleData\disparity_14-11-2022_14-42-13.npy")
-    left_frame_in = np.load(r"SampleData\left_14-11-2022_14-42-13.npy")
-    right_frame_in = np.load(r"SampleData\right_14-11-2022_14-42-13.npy")
-    RGB_frame_in = np.load(r"SampleData\RGB_14-11-2022_14-42-13.npy")
-    points_in = np.load(r"SampleData\spatials_14-11-2022_14-19-11.npy")
-
-    # Analyse the given depth map
-    print(f"Data-type of depth_map_in: {depth_map_in.dtype}")
-    print(f"Shape of depth_map_in: {depth_map_in.shape}")
-    print(f"Number of Dimensions of depth_map_in: {depth_map_in.ndim}")
-    print(f"Unique values of depht_map_in: {pd.unique(np.concatenate(depth_map_in))}")
-    print(f"Min value: {np.min(depth_map_in)}")
-    print(f"Max value: {np.max(depth_map_in)}")
-
-    # TODO: If general normalization is wanted, do it here!
-
-    # Crop depth map to view only the single test objects
-    depth_map_tesa = depth_map_in[300:480,700:870]
-    depth_map_ell = depth_map_in[190:330,570:750]
-    depth_map_iso = depth_map_in[150:430,300:600]
-
-    depth_map_objects = [depth_map_tesa, depth_map_ell, depth_map_iso]
-    objects = 3
-
-    # Plot the normalized depth map, RGB frame and the test objects
-    rows = 1
-    cols = objects +2
-
-    depth_map_vis = cv2.normalize(depth_map_in, None, 255, 0, cv2.NORM_INF, cv2.CV_8UC1)
-    depth_map_vis = cv2.equalizeHist(depth_map_vis)
-    depth_map_vis = cv2.applyColorMap(depth_map_vis,cv2.COLORMAP_HOT)
-
-    fig, axs =plt.subplots(rows, cols)
-    fig.suptitle('3 relevant objects in the depth_map')
-    for i,img in enumerate(depth_map_objects):
-        # Normalization of the depth_map for visualization purposes
-        img = cv2.normalize(img, None, 255, 0, cv2.NORM_INF, cv2.CV_8UC1)
-        img = cv2.equalizeHist(img)
-        img = cv2.applyColorMap(img,cv2.COLORMAP_HOT) # Change the color maps (available maps see in docs of opencv); HOT or JET
-        object = axs[i].imshow(img)
-        #fig.colorbar(object,ax=axs[i], orientation='vertical', fraction=0.1)
-    RGB_frame_in = cv2.cvtColor(RGB_frame_in,cv2.COLOR_BGR2RGB)
-    axs[3].imshow(RGB_frame_in)
-    axs[4].imshow(depth_map_vis)
-    fig.subplots_adjust(wspace=0.5)
-
-    plt.show()
 
     # Reading point cloud from .ply file
     pcd_load = o3d.io.read_point_cloud(r"SampleData\spatials_14-11-2022_14-42-13.ply")
@@ -93,12 +37,16 @@ if __name__ == "__main__":
     
     print(points)
 
-    XMIN = 20
-    XMAX = 300
 
-    YMIN = -25
-    YMAX = 40
+    # Lötlitze Coordinates
+    XMIN = 45
+    XMAX = 70
 
+    YMIN = 0
+    YMAX = 25
+
+    ZMIN = 100
+    ZMAX = 355
 
     # crop z axis
     mask_z =np.where((points[:,2] > ZMIN) & (points[:,2] < ZMAX))[0] 
@@ -116,6 +64,7 @@ if __name__ == "__main__":
 
     o3d.visualization.draw_geometries([cropped_pcd], window_name='timon_and_christoph_croppen')
 
+    o3d.io.write_point_cloud("cropped_pcd_loetlitze_exp.ply", cropped_pcd)
 
     # display_inlier_outlier(pcd_load,mask)
 
